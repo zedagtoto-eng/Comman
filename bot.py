7854f0e1852c2b2cf59597dcb4b8a5be7b9cfcb2
Drei <zedagtoto@gmail.com>
2026-09-01 17:35:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -524,15 +524,14 @@ note_embed = discord.Embed(
         
 view = ApplicationView(member.id)
 
-    # Main embed
-    await ctx.send(
+# Main embed
+await ctx.send(
         content=member.mention,
         embed=embed,
         allowed_mentions=discord.AllowedMentions(users=True)
-    )
-
-    # Note embed + Accept/Decline buttons
-    await ctx.send(
+)
+# Note embed + Accept/Decline buttons
+await ctx.send(
         embed=note_embed,
         view=view
     )
